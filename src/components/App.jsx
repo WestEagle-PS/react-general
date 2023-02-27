9ad27cabe7e11e2a5d49d4049a67dbee30eaa36a
Anton Orlov <anton_orlov@ukr.net>
2023-02-27 22:52:25 +0200

    confirm component Timer

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Timer from './Timer/Timer';
+import TimerWithDisable from './Timer/TimerWithDisable/TimerWithDisable';
+
+export const App = () => {
+  return (
+    <>
+      <Timer />
+      <TimerWithDisable />
+    </>
+  );
+};
+export default App;
