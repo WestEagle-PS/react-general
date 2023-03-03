e431b077d8ea8f55245fd2d1cb87354e552a81c0
Anton Orlov <anton_orlov@ukr.net>
2023-03-03 13:10:50 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
+import { Link } from 'react-router-dom';
 import Timer from './Timer/Timer';
-import TimerWithDisable from './Timer/TimerWithDisable/TimerWithDisable';
+import TimerWithDisable from './TimerWithDisable/TimerWithDisable';
 
 export const App = () => {
   return (
